3fd5ade97d7e8b116c9770ac3575f01c5f05e35d
kgytis <kaziukon.gytis@gmail.com>
2022-10-13 08:39:11 +0300

    first commit

---
+++ vue/TaskModal.vue
@@ -0,0 +1,81 @@
+<template>
+  <a-modal :visible="toggleModal" footer @cancel="closeModal">
+    <vuci-form uci-config="task_one" :key="counter" @applied="closeModal">
+      <vuci-named-section
+        uci-config="task_one"
+        :name="sectionId"
+        type="test"
+        :collapsible="false"
+        v-slot="{ s }"
+      >
+        <vuci-form-item-select
+          :uci-section="s"
+          label="Protocol"
+          name="protocol"
+          :options="['static', 'dhcp']"
+          initial="dhcp"
+        >
+        </vuci-form-item-select>
+
+        <vuci-form-item-input
+          :uci-section="s"
+          label="Interface name:"
+          name="name"
+          rules="hostname"
+        />
+        <vuci-form-item-input
+          :uci-section="s"
+          label="Address"
+          name="address"
+          rules="ipaddr"
+          depend="protocol == 'static'"
+        />
+        <vuci-form-item-input
+          :uci-section="s"
+          label="Netmask"
+          name="netmask"
+          rules="netmask4"
+          depend="protocol == 'static'"
+        />
+        <vuci-form-item-input
+          :uci-section="s"
+          label="Gateway"
+          name="gateway"
+          rules="ipaddr"
+          depend="protocol == 'static'"
+        />
+        <vuci-form-item-list
+          :uci-section="s"
+          label="DNS"
+          name="dns"
+          depend="protocol == 'static'"
+        />
+      </vuci-named-section>
+    </vuci-form>
+  </a-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    toggleModal: {
+      type: Boolean,
+      required: true
+    },
+    counter: {
+      type: Number,
+      required: true
+    },
+    sectionId: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['closeModal'],
+  methods: {
+    closeModal () {
+      this.$emit('closeModal')
+    }
+  }
+}
+</script>
